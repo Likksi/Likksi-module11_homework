c7e7577a491923de6a92c1f0aacc9b81567c4372
Likksi <153167133+Likksi@users.noreply.github.com>
2024-02-08 14:45:54 +0300

    Update Function.js
    Реализация функции

--- TASK2/Function/Function.js
+++ TASK2/Function/Function.js
@@ -1,7 +1,9 @@
 function getPercents(percent, number) {
+    if (typeof percent !== 'number' || typeof number !== 'number') {
+        console.error("Ошибка: Оба аргумента должны быть числами");
+        return NaN; // Возвращаем NaN в случае ошибки
+    }
   
     let result = (percent / 100) * number;
     return result;
 }
-
-console.log(getPercents(30, 200));
